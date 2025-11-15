c4a6017f52f0a1436ece9980a7eb30341eebcf5a
merisanatalia <sasamerisa05@gmail.com>
2025-11-15 18:35:43 +0700

    pertemuan 15 nov

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ import express from "express";
 import api from "./route/api.js";
 import database from "./config/database.js"
 import movieModel from "./models/movieModel.js";
+import cors from "cors"
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 
 app.use('/api',api)
